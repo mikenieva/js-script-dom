fbd9ddc0348fe768b0a20c20102baca3271f40b9
Mike Nieva <m@mikenieva.com>
2023-12-12 16:49:01 -0600

    Elementos de la plantilla

--- script.js
+++ script.js
@@ -26,49 +26,7 @@ function renderNoSongs() {
 
 // agrega el código HTML de la canción dentro del elemento songs-container
 function addSong(artistValue, titleValue) {
-  const trackContainer = document.createElement("div"); // <div></div>
-  trackContainer.classList.add("song"); // <div class="song"></div>
-
-  const artistElement = document.createElement("h4"); // <h4></h4>
-  artistElement.classList.add("song__artist"); // <h4 class="song__artist"></h4>
-  artistElement.textContent = artistValue; // <h4 class="song__artist">The Beatles</h4>
-
-  const titleElement = document.createElement("p");
-  titleElement.classList.add("song__title");
-  titleElement.textContent = titleValue;
-
-  const likeButtonElement = document.createElement("button");
-  likeButtonElement.classList.add("song__like");
-
-  trackContainer.append(artistElement, titleElement, likeButtonElement);
-  /** ESTE ES EL RESULTADO DEL APPEND
-   * <div class="song">
-   *    <h4 ...>...</h4>
-   *    <p...>..</p...>
-   *    <button>....</button>
-   * </div>
-   */
-
-  songsContainer.append(trackContainer);
-
-  //   songsContainer.insertAdjacentHTML(
-  //     "beforeend",
-  //     `
-  //   <div class="song">
-  // <img
-  //   src="https://invalid_url"
-  //   onerror="alerta('pero sobre todo, samy es mi héroe')"
-  // />
-  //     <h4 class="song__artist">${
-  //       artistValue /* estos son datos del usuario */
-  //     }</h4>
-  //     <p class="song__title">${
-  //       titleValue /* estos también son datos del usuario */
-  //     }</p>
-  //     <button class="song__like"></button>
-  //   </div>
-  // `
-  //   );
+  const songsTemplate = document.querySelector("#song-template");
 }
 
 // B. EVENTOS
